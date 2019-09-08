6de7ab7f5735c9067ff3c86f1b76ffe87f5889ed
marcel fonseca <marcelsf23br@gmail.com>
2019-09-08 19:42:16 -0300

    create session broadcast method

--- app/models/session.rb
+++ app/models/session.rb
@@ -1,4 +1,12 @@
 class Session < ApplicationRecord
   has_many :messages
   has_many :replies
+
+  def broadcast
+    serialized_data = ActiveModelSerializers::Adapter::Json.new(
+      ConversationSerializer.new(self)
+    ).serializable_hash
+    ActionCable.server.broadcast 'sessions_channel', serialized_data
+    head :ok
+  end
 end
